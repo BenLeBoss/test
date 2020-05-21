4df72933b8b94e4139ec493f40c0d8b1d2b6da40
BenLeBoss <49184923+BenLeBoss@users.noreply.github.com>
2020-05-21 19:50:09 +0200

    Add files via upload

--- client.c
+++ client.c
@@ -19,6 +19,36 @@
 int main(void)
 {
 
+  //déclaration de la socket
+  struct sockaddr_in addr_server;
+  int socketS = socket(AF_INET, SOCK_DGRAM, 0);
+  if (socketS == -1){
+    perror("socket error\n");
+    exit(1);
+  }
+
+  //récupération de l'identifiant du serveur
+  char hostname[50];
+  int val;
+  val = gethostname(hostname, sizeof(hostname));
+  if (val == -1){
+    perror("Unable to get the hostname");
+    exit(1);
+  }
+
+  struct hostent *serveur_host;
+  serveur_host = gethostbyname(hostname);
+  printf("hostname : %s\n", hostname);
+
+
+  //création de la socket du serveur
+  bzero(&addr_server, sizeof(struct sockaddr_in));
+  addr_server.sin_family = AF_INET;
+  addr_server.sin_port = htons(4000);
+  memcpy(&addr_server.sin_addr.s_addr, serveur_host->h_addr, serveur_host->h_length);
+
+  //envoie un message
+  char *msg = "bonjour";
   socklen_t lg = sizeof(struct sockaddr_in);
   int nb_octets = sendto (socketS, msg, strlen(msg)+1, 0, (struct sockaddr*)&addr_server, lg);
   if (nb_octets == -1){
@@ -32,5 +62,3 @@ int main(void)
 
   return 0;
 }
-
-frz"aerzfaerzfr"aefaerzsfd
